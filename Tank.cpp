4d36e1166babeedf520ed98d0c55b12261c633b6
eventyrer1 <142988726+eventyrer1@users.noreply.github.com>
2025-10-02 09:50:54 +0200

    changed the model from tank to car, and added a plane

--- Tank.cpp
+++ Tank.cpp
@@ -1,9 +1,9 @@
-// C++
+
 #include "Tank.hpp"
 #include "threepp/loaders/AssimpLoader.hpp"
 
 using namespace threepp;
-
+//used ai for much of this code
 Tank::Tank(std::shared_ptr<Object3D> model)
     : model_(std::move(model))
 {
@@ -14,6 +14,6 @@ std::shared_ptr<Tank> Tank::create(const std::filesystem::path& path) {
     AssimpLoader loader;
     auto model = loader.load(path);
     if (!model) return nullptr;
-    model->scale.multiplyScalar(10);
+    model->scale.multiplyScalar(3);
     return std::make_shared<Tank>(model);
 }
--- main.cpp
+++ main.cpp
@@ -5,6 +5,34 @@
 
 using namespace threepp;
 
+namespace {
+    auto createWireframe(const BufferGeometry& geometry) {
+
+        const auto material = LineBasicMaterial::create({{"color", Color::black}});
+        return LineSegments::create(WireframeGeometry::create(geometry), material);
+    }
+
+    void updateGroupGeometry(Mesh& mesh, const PlaneGeometry::Params& params) {
+
+        const auto geometry = PlaneGeometry::create(params);
+        mesh.setGeometry(geometry);
+
+        mesh.children[0]->removeFromParent();
+        mesh.add(createWireframe(*geometry));
+    }
+
+    auto createMesh(const PlaneGeometry::Params& params) {
+
+        const auto geometry = PlaneGeometry::create(params);
+        const auto material = MeshBasicMaterial::create();
+        material->side = Side::Double;
+
+        auto mesh = Mesh::create(geometry, material);
+        mesh->add(createWireframe(*geometry));
+
+        return mesh;
+    }
+}
 
 int main() {
     Canvas canvas{Canvas::Parameters().title("Tank").size({1280, 720}).antialiasing(8)};
@@ -29,12 +57,12 @@ int main() {
     auto light2 = AmbientLight::create(0xffffff, 1.f);
     scene->add(light2);
     try {
-        auto tank = Tank::create(std::filesystem::path("Data/Models/m26.dae"));
+        auto tank = Tank::create(std::filesystem::path("Data/Models/Car.dae"));
         if (tank) {
-            tank->position.set(0, 0, 0);
+            tank->position.set(0, 5, 0);
             scene->add(tank);
         } else {
-            std::cerr << "Failed to load `Data/Models/m26.dae`\n";
+            std::cerr << "Failed to load `Data/Models/Car.dae`\n";
         }
     } catch (const std::exception& ex) {
         std::cerr << "Exception during tank loading: " << ex.what() << std::endl;
@@ -42,6 +70,11 @@ int main() {
         std::cerr << "Unknown exception during tank loading." << std::endl;
     }
 
+        PlaneGeometry::Params params{1000,1000};
+
+        const auto mesh = createMesh(params);
+        scene->add(mesh);
+       mesh->rotation.set(-math::PI / 2, 0, 0);
     Clock clock;
     canvas.animate([&]() {
         const auto dt = clock.getDelta();
