35288e1c555f4eccaf39f237906c1a89e229b238
eventyrer1 <142988726+eventyrer1@users.noreply.github.com>
2025-10-04 15:24:41 +0200

    Finished keylistener and started on actually doing functions

--- src/keylisteners/car_keylistener.cpp
+++ src/keylisteners/car_keylistener.cpp
@@ -1,5 +1,44 @@
-
 #include "car_keylistener.hpp"
 
 using namespace threepp;
 
+CarKeyListener::CarKeyListener() {
+    keymap[Key::W] = CarAction::ACCELERATE;
+    keymap[Key::S] = CarAction::DECELERATE;
+    keymap[Key::A] = CarAction::TURN_LEFT;
+    keymap[Key::D] = CarAction::TURN_RIGHT;
+}
+
+void CarKeyListener::onKeyPressed(KeyEvent evt) {
+    keys_pressed.insert(evt.key);
+    keys_released.erase(evt.key);
+}
+
+void CarKeyListener::onKeyReleased(KeyEvent evt) {
+    keys_pressed.erase(evt.key);
+    keys_released.insert(evt.key);
+}
+
+bool CarKeyListener::isKeyPressed(Key key) const {
+    return keys_pressed.count(key);
+}
+
+bool CarKeyListener::isKeyReleased(Key key) const {
+    return keys_released.count(key);
+}
+
+std::set<CarKeyListener::CarAction> CarKeyListener::determine_action() {
+    std::set<CarAction> actions;
+
+    for (auto& [key, action] : keymap) {
+        if (isKeyPressed(key)) {
+            actions.insert(action);
+        }
+    }
+
+    return actions;
+}
+
+void CarKeyListener::bindKey(Key key, CarAction action) {
+    keymap[key] = action;
+}
