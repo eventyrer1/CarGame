518401c2a37426dea922ff4182a8c507871d8b39
eventyrer1 <142988726+eventyrer1@users.noreply.github.com>
2025-10-06 11:59:10 +0200

    fixed the camera for the car

---
+++ src/setups/setup.cpp
@@ -0,0 +1,19 @@
+//
+// Created by evest on 06.10.2025.
+//
+
+#include "setup.hpp"
+#include "threepp/threepp.hpp"
+using namespace threepp;
+
+void setupScene(Scene &scene) {
+
+    auto light1 = HemisphereLight::create();
+    light1->intensity = 0.8f;
+    scene.add(light1);
+
+    auto grid = GridHelper::create(1000, 1000);
+    scene.add(grid);
+    scene.background = Color::aliceblue;
+
+}
