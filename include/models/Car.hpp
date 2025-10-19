6a8c0725b4ba6422c0a60560cecf507e7cb435fa
eventyrer1 <142988726+eventyrer1@users.noreply.github.com>
2025-10-19 19:59:58 +0200

    added testing, and CI

--- include/models/Car.hpp
+++ include/models/Car.hpp
@@ -2,14 +2,13 @@
 #define TANK_HPP
 
 #include <memory>
-#include <filesystem>
 #include "threepp/threepp.hpp"
-#include <keylisteners/car_keylistener.hpp>
+#include "keylisteners/car_keylistener.hpp"
+#include "collision/collidable.hpp"
 
 using namespace threepp;
 
-
-class Car : public Object3D {
+class Car : public Object3D, public Collidable {
 public:
     explicit Car(std::shared_ptr<Object3D> model);
 
@@ -20,45 +19,60 @@ public:
     );
 
     std::shared_ptr<Object3D> getModel() const { return model_; }
-    //inspirert av sphero.hpp fra threepp eksempel
+
     PerspectiveCamera &camera();
 
-    // CHANGED: Return Sphere instead of Box3
+    static std::shared_ptr<Car> createDummyCar() {
+        return std::make_shared<Car>(nullptr);
+    }
     Sphere getBoundingSphere() const {
         return boundingSphere_;
     }
 
-    // CHANGED: Collision with Box3 (for trees)
+
     bool collidesWith(const Box3& otherBox) const {
         return boundingSphere_.intersectsBox(otherBox);
     }
 
-    // ADDED: Collision with other sphere (for future car-to-car)
+
     bool collidesWith(const Sphere& otherSphere) const {
         return boundingSphere_.intersectsSphere(otherSphere);
     }
 
-    // Add method to enable/update debug visualization
+
     void setHitboxVisualization(bool enabled, Scene* scene = nullptr);
     void updateHitboxVisualization();
+    
+
+    bool checkCollision(const Collidable& other) const override;
+    const Sphere* getSphere() const override { return &boundingSphere_; }
+    Vector3 getPosition() const override { return model_ ? model_->position : Vector3(); }
+    void onCollision(Collidable* other) override;
+
+
+    //since this is only for testing and is very small i decided to have the entire getter and setter in the header
+    float getSpeed() const { return speed_; }
+    float getMaxSpeed() const { return maxSpeed_; }
+    void setSpeed(float speed) { speed_ = speed; }
 
 private:
     int speed_ = 10;
-    int maxSpeed_ = 100;
+    int maxSpeed_ = 200;
     int acceleration_ = 100;
     int rotationSpeed_ = 2;
     float angle_ = 0.0;
     std::shared_ptr<Object3D> model_;
     std::unique_ptr<PerspectiveCamera> camera_;
 
-    // CHANGED: Use Sphere instead of Box3
+
     Sphere boundingSphere_;
 
-    // For visualization - we'll create a sphere mesh
+
     std::shared_ptr<Mesh> boundingSphereHelper_;
     Scene* scene_ = nullptr;
 
     void updateBoundingSphere();
+    void handleCollisionResponse(Collidable* other);
 };
 
 #endif // TANK_HPP
