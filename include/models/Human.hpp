9094a978a7ef2323c1c2f390b2e79a98850be3da
eventyrer1 <142988726+eventyrer1@users.noreply.github.com>
2025-11-05 19:07:29 +0100

    Added sound

--- include/models/Human.hpp
+++ include/models/Human.hpp
@@ -5,18 +5,22 @@
 #include "threepp/audio/Audio.hpp"
 
 namespace threepp {
-
     class Human : public SpawnableObject {
     public:
-        Human(std::shared_ptr<Object3D> model, AudioListener& listener);        using SpawnableObject::SpawnableObject;
-        static std::shared_ptr<Human> create(std::shared_ptr<Object3D> model, AudioListener& listener);
+        Human(std::shared_ptr<Object3D> model,
+              AudioListener *listener,
+              const std::string &soundPath);
+        static std::shared_ptr<Human> create(std::shared_ptr<Object3D> model,
+                                             AudioListener *listener,
+                                             const std::string &soundPath);
         void computeBoundingBox() override;
-        void onCollision(::Collidable* other) override;
+        void onCollision(::Collidable *other) override;
 
     private:
         std::shared_ptr<PositionalAudio> collisionSound_;
+
         void handleCollisionResponse(Collidable *other);
     };
-
 }
-#endif //CARGAME_HUMAN_HPP
+
+#endif // CARGAME_HUMAN_HPP
--- include/models/SpawnableObject.hpp
+++ include/models/SpawnableObject.hpp
@@ -11,8 +11,6 @@ namespace threepp {
 
     class SpawnableObject : public Collidable {
     public:
-        //explicit SpawnableObject(std::shared_ptr<Object3D> model, const Vector3& position = Vector3());
-        //static std::shared_ptr<SpawnableObject> create(std::shared_ptr<Object3D> model);
 
        virtual void setRandomPosition(float minX, float maxX, float minZ, float maxZ);
 
@@ -20,18 +18,13 @@ namespace threepp {
        virtual void setHitboxVisualization(bool enabled, Scene* scene = nullptr);
        virtual void updateHitboxVisualization();
 
-        // --- Collidable interface ---
 
-
-        //void onCollision(Collidable* other) {}
-
-       //virtual void updateCollisionBox();
     protected:
         SpawnableObject() = default;
         Scene* scene_ = nullptr;
         std::shared_ptr<Object3D> visualBounds_;
 };
 
-} // namespace threepp
+}
 
 #endif // CARGAME_WORLDOBJECT_HPP
